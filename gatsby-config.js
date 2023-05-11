6a33b28e5436bb94bb0de0f0e43f7a46a3ebc966
Keshav Worathur <keshavworathur@Keshavs-MacBook-Pro.local>
2023-05-11 13:58:01 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Keshavs Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
