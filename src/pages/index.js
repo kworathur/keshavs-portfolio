985f0706a72699f290edebdf3855abe2c593fd7a
Keshav Worathur <keshavworathur@Keshavs-MacBook-Pro.local>
2023-05-11 21:54:00 -0500

    Add description to site metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
 import * as React from 'react'
+import Layout from '../components/Layout/layout';
 
 
 const IndexPage = () => {
   return (
-    <main>
+    <Layout>
       <h1>Keshav's Portfolio Site</h1>
       <p>Fourth-Year Computer Science Specialist at UofT.</p>
-    </main>
+    </Layout>
   )
 }
 
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => (
+  <>
+    <title>Keshav Worathur</title>
+    <meta name="description" content="Keshav Worathur's Portfolio. Keshav Worathur is a fourth year computer science specialist at Uoft" />
+  </>
+);
+
 
 export default IndexPage
