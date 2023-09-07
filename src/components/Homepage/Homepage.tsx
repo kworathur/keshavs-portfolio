ea75fbc6061b54ccf06b54ecb9886c1b83245701
Keshav Worathur <keshavworathur@gmail.com>
2023-09-07 08:30:11 -0400

    feat: create homepage component
    The homepage includes a brief introduction with social media links and
    a resume with expandable sections.

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,29 +1,38 @@
-import React, {useState, ReactElement} from "react"
+import React, { useState, ReactElement } from "react";
 
-import Links, {Locations} from "../Links/Links"
+import SEO from '../SEO/SEO'
+import Links, { Locations } from "../Links/Links";
+// import Arrow from "../Arrow/Arrow";
 
+import Education from "../Resume/Education/Education";
+import Experience from "../Resume/Experience/Experience";
+import Projects from "../Resume/Projects/Projects";
+import Extracurriculars from "../Resume/Activities/Activities";
+
+import Intro from "../Intro/Intro";
+import Footer from "../Footer/Footer"
+import { UNIVERSITY_YEAR_AS_STRING } from "../../data/Constants";
+
+import * as styles from "./Homepage.module.scss"
+import { StaticImage } from "gatsby-plugin-image";
 
 const Homepage: React.FC = (): ReactElement => {
-
-    
-
 
     return (
         <>
             <Links location={Locations.HOMEPAGE} />
-
+            <SEO title="Home" />
+            <Intro />
             <div className="resume">
-                {/* <Education />
+                <Education />
                 <Experience />
                 <Projects />
-                <Extracurriculars />
-                <Footer /> */}
+                {/* <Extracurriculars /> */}
+                <Footer />
+        
             </div>
-        
-        
         </>
-    )
+    );
+};
 
-}
-
-export default Homepage 
+export default Homepage;
