5bed0526eaa9e9ca9e0c666592fd54d39724441e
Keshav Worathur <keshavworathur@gmail.com>
2026-04-28 21:39:50 -0400

    Squashed commit of the following:
    commit 7fa5da6c86cfe4d12620966c6452c9de592a903a
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Tue Apr 28 21:39:24 2026 -0400
    
        fix: remove wip blog post
    
    commit 404bd0a2b752f37881e4f9527fd4359dc39b2ec5
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Tue Apr 28 21:38:25 2026 -0400
    
        styles: use a more modern color theme
    
    commit f4e692ee2df4902256b8aa911ca0b8aee908141d
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Tue Apr 28 17:28:47 2026 -0400
    
        feat(blog): polish intro section
    
    commit 37b74daa6bda377c27120e159cd0d7d2d90c56c0
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Sun Apr 26 14:20:23 2026 -0400
    
        content: add dns reflection post
    
    commit ae4e1d8967f645ff7cf3f8b4cde43caba134864b
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Sun Apr 19 21:21:51 2026 -0400
    
        styles: adjust logo padding
    
    commit dc050f2bad943dcc4db5644bb3de06141e7ae723
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Sun Apr 19 21:21:26 2026 -0400
    
        fix: point gatsby-source to correct folder
    
    commit 5f63d16089c1bf7c3fcf329cc149585e488f7da4
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Sun Apr 19 21:15:29 2026 -0400
    
        feat: implement blog post list
    
    commit 4b2a31d5c31c04baebb819f453f8e0564f915fa5
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Sat Apr 18 12:21:52 2026 -0400
    
        feat: add intro section
    
    commit ecb7e0944923b8cacea8d34a84c6a7b7590eda4d
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Sat Apr 18 11:18:35 2026 -0400
    
        chore: upgrade deps to latest versions, downgrade to react 18
    
    commit edfe2b798ce1c3abb1f1b20ebeb6f246d3548395
    Author: Keshav Worathur <keshavworathur@gmail.com>
    Date:   Sat Apr 18 11:17:44 2026 -0400
    
        styles: use custom typography

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,153 +1,95 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-module.exports = {
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
   siteMetadata: {
-        title: "kworathur.com",
-        author: {
-            name: "Keshav Worathur",
-            summary: `an aspiring data engineer completing his master's at the Georgia Institute of Technology in Atlanta, GA.`,
-        },
-        description:
-            "Keshav Worathur's developer portfolio.",
-        siteUrl: "https://kworathur.com",
-        social: {
-            twitter: "kworathur",
-        },
+    title: 'kworathur.com',
+    author: {
+      name: 'Keshav Worathur',
+      summary: `an aspiring software engineer completing his master's at the Georgia Institute of Technology in Atlanta, GA.`,
     },
+    description: "Keshav Worathur's developer portfolio.",
+    siteUrl: 'https://kworathur.com',
+    social: {
+      linkedin: 'kworathur',
+    },
+  },
   trailingSlash: `never`,
   plugins: [
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-image`,
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-                rule: {
-                    include: `${__dirname}/content/assets`,
-                },
-            },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: `${__dirname}/content/assets`,
         },
-        {
-            resolve: `gatsby-source-filesystem`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                path: `${__dirname}/content/projects`,
-                name: `projects`,
+              maxWidth: 590,
+              linkImagesToOriginal: true,
+              showCaptions: ['title', 'alt'],
+              quality: 50,
+              wrapperStyle: `margin: 0 auto; text-align: center; font-style: italic;`,
             },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-                path: `${__dirname}/content/assets`,
-                name: `assets`,
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                            linkImagesToOriginal: true,
-                            showCaptions: ["title", "alt"],
-                            quality: 50,
-                            wrapperStyle: `margin: 0 auto; text-align: center; font-style: italic;`,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    `gatsby-remark-prismjs`,
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`,
-                ],
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-feed`,
-            options: {
-                query: `{
-                    site {
-                        siteMetadata {
-                            title
-                            description
-                            siteUrl
-                            site_url: siteUrl
-                        }
-                    }
-                }`,
-                feeds: [
-                    {
-                        serialize: ({ query: { site, allMarkdownRemark } }) => {
-                            return allMarkdownRemark.nodes.map(node => {
-                                return Object.assign({}, node.frontmatter, {
-                                    description: node.excerpt,
-                                    date: node.frontmatter.date,
-                                    url:
-                                        site.siteMetadata.siteUrl +
-                                        node.fields.slug,
-                                    guid:
-                                        site.siteMetadata.siteUrl +
-                                        node.fields.slug,
-                                    custom_elements: [
-                                        { "content:encoded": node.html },
-                                    ],
-                                });
-                            });
-                        },
-                        query: `
-                        {
-                            allMarkdownRemark(
-                                sort: { order: DESC, fields: [frontmatter___date] },
-                            ) {
-                                nodes {
-                                    excerpt
-                                    html
-                                    fields {
-                                        slug
-                                    }
-                                    frontmatter {
-                                        title
-                                        date
-                                    }
-                                }
-                            }
-                        }`,
-                        output: "/rss.xml",
-                        title: "Keshav's Blog",
-                        match: "^/blog/",
-                    },
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Keshav Worathur's Blog`,
-                short_name: `Keshav's Blog`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#202020`,
-                display: `minimal-ui`,
-                icon: `content/assets/kw-favicon.svg`,
-            },
-        },
-        `gatsby-plugin-react-helmet`,
-        "gatsby-plugin-dark-mode",
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography`,
-            
-            },
-        },
-        // Enable PWA and offline functionality
-        `gatsby-plugin-offline`,
-    ],
-}
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Keshav Worathur's Blog`,
+        short_name: `Keshav's Blog`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#202020`,
+        display: `minimal-ui`,
+        icon: `content/assets/kw-favicon.svg`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-dark-mode',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // Enable PWA and offline functionality
+    `gatsby-plugin-offline`,
+  ],
+};
+
+export default config;
--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,92 +1,105 @@
-import React, { ReactElement } from "react";
-import { Link } from "gatsby";
-import Toggle from "../Toggle/Toggle";
-import * as styles from "./Links.module.scss";
-import {ThemeToggler} from 'gatsby-plugin-dark-mode';
-
+import React, { ReactElement } from 'react';
+import { Link } from 'gatsby';
+import Toggle from '../Toggle/Toggle';
+import * as styles from './Links.module.scss';
+import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 
 export enum Locations {
-    HOMEPAGE,
-    POSTS,
+  HOMEPAGE,
+  POSTS,
 }
 
 export const Destinations = [
-    { name: "resume", path: "https://uploads.kworathur.com/keshav_worathur_resume.pdf", external: true },
+  {
+    name: 'resume',
+    path: 'https://uploads.kworathur.com/keshav_worathur_resume.pdf',
+    external: true,
+  },
+  {
+    name: 'blog',
+    path: '/blog',
+  },
 ];
 
 interface InnerLinksProps {
-    location: Locations;
+  location: Locations;
 }
 
 type ToggleThemeFn = {
-    (checked: string): void;
+  (checked: string): void;
 };
 
 export interface ThemeTogglerProps {
-    theme: unknown;
-    toggleTheme: ToggleThemeFn;
+  theme: unknown;
+  toggleTheme: ToggleThemeFn;
 }
 
 const InnerLinks = (props: InnerLinksProps): ReactElement => {
-    const { location } = props;
-    const prefix = location === Locations.HOMEPAGE ? "homepage" : "";
+  const { location } = props;
 
-    return (
-
-            <ThemeToggler>
-                            {({ theme, toggleTheme }: ThemeTogglerProps) =>  (
-                                       <div className={styles[`${prefix}Links`]}>
-                                            <Link className={styles.headerLink} to={"/"}><h1 className={styles.logo}>kW</h1></Link>
-                                            <div className={styles.linksAndLights}>
-                                            {Destinations.map((d, index) => {
-                                                    return d.external ? (
-                                                        <a
-                                                            key={index}
-                                                            className={`${styles[`${prefix}Link`]} ${styles.navLink}`}
-                                                            href={d.path}
-                                                            target="_blank"
-                                                            rel="noreferrer"
-                                                        >
-                                                            {d.name}
-                                                        </a>
-                                                    ) : (
-                                                        <Link
-                                                            key={index}
-                                                            className={`${styles[`${prefix}Link`]} ${styles.navLink}`}
-                                                            to={d.path}
-                                                        >
-                                                            {d.name}
-                                                        </Link>
-                                                    );
-                                            })}
-                                        <Toggle location={location} theme={theme} toggleTheme={toggleTheme}/>
-                                            </div>
-                                        </div>
-                                )
-                            }
-            </ThemeToggler>
-
-    );
+  return (
+    <ThemeToggler>
+      {({ theme, toggleTheme }: ThemeTogglerProps) => (
+        <div className={styles[`homepageLinks`]}>
+          <Link className={styles.headerLink} to={'/'}>
+            <h1 className={styles.logo}>kW</h1>
+          </Link>
+          <div className={styles.linksAndLights}>
+            {Destinations.map((d, index) => {
+              return d.external ? (
+                <a
+                  key={index}
+                  className={`${styles[`homepageLink`]} ${styles.navLink}`}
+                  href={d.path}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {d.name}
+                </a>
+              ) : (
+                <Link
+                  key={index}
+                  className={`${styles[`homepageLink`]} ${styles.navLink}`}
+                  to={d.path}
+                >
+                  {d.name}
+                </Link>
+              );
+            })}
+            <Toggle
+              location={location}
+              theme={theme}
+              toggleTheme={toggleTheme}
+            />
+          </div>
+        </div>
+      )}
+    </ThemeToggler>
+  );
 };
 
 interface LinksProps {
-    location: Locations;
+  location: Locations;
 }
 
 const Links = (props: LinksProps): ReactElement => {
-    const { location }  = props;
-    switch (location) {
-        case Locations.HOMEPAGE:
-            return (
-                <div className={styles.homepageWrapper}>
-                    <InnerLinks location={location} />
-                </div>
-            );
-        case Locations.POSTS:
-            return <InnerLinks location={location} />;
-        default:
-            throw new Error(`Invalid location given in Links.tsx`);
-    }
+  const { location } = props;
+  switch (location) {
+    case Locations.HOMEPAGE:
+      return (
+        <div className={styles.homepageWrapper}>
+          <InnerLinks location={location} />
+        </div>
+      );
+    case Locations.POSTS:
+      return (
+        <div className={styles.homepageWrapper}>
+          <InnerLinks location={location} />
+        </div>
+      );
+    default:
+      throw new Error(`Invalid location given in Links.tsx`);
+  }
 };
 
 export default Links;
